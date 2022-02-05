a009338848595ba506ef64d1e073c15e12c1aab2
bchung1201 <bryanchung2005@gmail.com>
2022-02-04 23:25:58 -0500

    edits

---
+++ projectWithClaire/src/Othello.java
@@ -0,0 +1,32 @@
+public class Othello {
+    int[][] board;
+    boolean curPlayer;
+    int blackCount;
+    int whiteCount;
+    private final int WHITE = 1;
+    private final int BLACK = 2;
+
+    public Othello(){
+        board = new int[5][5];
+        curPlayer = true;
+        board[3][3] = WHITE;
+        board[4][4] = WHITE;
+        board[3][4] = BLACK;
+        board[4][3] = BLACK;
+        blackCount = 2;
+        whiteCount = 2;
+    }
+
+    private void makeMove(int i, int j) {
+        //somehow need to get i and j from the GUI/input
+
+        if (i == 0 && ((j != 0) || (j != board.length-1))) {
+            while(j-1 > 1 && board[i][j-1] == BLACK) {
+
+            }
+        }
+
+    }
+
+
+}
