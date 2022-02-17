414459047abfaaa7bc442d3500037d5cafc853ea
bchung1201 <bryanchung2005@gmail.com>
2022-02-16 23:24:08 -0500

    rt5rt5555456465465

---
+++ projectWithClaire/src/Frame.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class Frame extends JFrame{
+    public Frame(){
+        Color myGreen= new Color (71, 143, 95,255);
+        setVisible(true);
+        setResizable(false);
+        setSize(700,700);
+        setLayout(new GridLayout(8, 8, 1, 1));
+        getContentPane().setBackground(Color.BLACK);
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                JPanel panel = new JPanel();
+                panel.setBackground(myGreen);
+                add(panel);
+            }
+        }
+//        JButton button= new JButton();
+//        button.addActionListener(new ActionListener(){
+//            public void actionPerformed(ActionEvent e){
+//                button.setBackground (Color.YELLOW);
+//            }
+//        });
+//        add(button);
+
+
+    }
+}
